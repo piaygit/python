d77cbd92dadcb8a5012f27abc02738174abbce03
梦醉丶半生 <luo1yong23@163.com>
2016-06-23 22:03:05 +0800

    优化翻译

--- 练习代码_各种小轮子/有道翻译.py
+++ 练习代码_各种小轮子/有道翻译.py
@@ -58,36 +58,47 @@ class youdao_fanyi(object):
         return pargmas
 
     def _get_fanyi_json(self):
+        '''
+        执行get请求
+        :return:
+        '''
         dict_data = self._pargmas_data()
         text = raw_input("请输入需要翻译的单词：")
         dict_data['q'] = text
         result = requests.get(self.url, params=dict_data)
         self.errorCode = result.status_code
-        if self.errorCode == 0:
-            pass
-        elif self.errorCode == 20:
-            print ('要翻译的文本过长')
-            return self._get_fanyi_json()
-        elif self.errorCode == 30:
-            print ('无法进行有效的翻译')
-            return self._get_fanyi_json()
-        elif self.errorCode == 40:
-            print ('不支持的语言类型')
-            return self._get_fanyi_json()
-        elif self.errorCode == 50:
-            print ('无效的key')
-            return self._get_fanyi_json()
-        elif self.errorCode == 60:
-            print ('无词典结果，仅在获取词典结果生效')
-            return self._get_fanyi_json()
-        result = result.text
-        return result
+        if self.errorCode==200:
+            result = result.text
+            return result
 
     def analysis_json(self):
-        result_json_data=self._get_fanyi_json()
-        import json
-        result=json.loads(result_json_data)
-        print result['translation']
+        while True:
+            '''
+            获取翻译结果
+            :return:
+            '''
+            result_json_data=self._get_fanyi_json()
+            import json
+            result=json.loads(result_json_data)
+            status_code=result['errorCode']
+            if status_code == 0:
+                print result['translation'][0]
+            elif status_code == 20:
+                print ('要翻译的文本过长')
+                return self.analysis_json
+            elif status_code == 30:
+                print ('无法进行有效的翻译')
+                return self.analysis_json
+            elif status_code == 40:
+                print ('不支持的语言类型')
+                return self.analysis_json
+            elif status_code == 50:
+                print ('无效的key')
+                return self.analysis_json
+            elif status_code == 60:
+                print ('无词典结果，仅在获取词典结果生效')
+                return self.analysis_json
+
 
 
 app_key_data = {'key': '61555165', 'keyfrom': 'pythoncontent1111'}
